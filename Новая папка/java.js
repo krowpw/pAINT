fbfd697d8bf45e75d80f6d357eb7f44c76d92dfd
krowpw <130204567+krowpw@users.noreply.github.com>
2024-04-13 18:25:33 +1000

    Add files via upload

--- Новая папка/java.js
+++ Новая папка/java.js
@@ -1,15 +1,41 @@
-let block = document.querySelectorAll(".block")
-console.log(block)
+let block = document.querySelector(".bigbox")
+const blocksize = 20
+let s = (block.offsetWidth * block.offsetHeight) / (Math.pow(blocksize,2))
 
-
-for (let i = 0; i<9; i++) {
-    block[i].addEventListener("mouseenter", function(){
-    console.log("я пидорас", i)
-    block[i].classList.add("--active")
-    setTimeout(()=>{
-         block[i].classList.remove("--active")
-    })
-})
+// for (let i = 0; i<9; i++) {
+//     block[i].addEventListener("mouseenter", function(){
+//     console.log("я пидорас", i)
+//     block[i].classList.add("--active")
+//     setTimeout(()=>{
+//          block[i].classList.remove("--active")
+//     })
+// })
     
+let color = document.getElementById("color");
+console.log(color.value);
     
+// }
+for (let i = 0; i < s; i++) {
+    block.insertAdjacentHTML("afterbegin", '<div class="block"></div>')
 }
+
+let blocklist = document.querySelectorAll(".block")
+
+let isDraw = false
+block.addEventListener("mousedown", ()=>{
+    isDraw = true
+})
+block.addEventListener("mouseup", ()=>{
+    isDraw = false
+})
+blocklist.forEach( block => {
+    block.addEventListener("mouseenter", () => {
+        if (isDraw) {
+            block.style.backgroundColor = color.value ;
+        }
+    
+    }
+)
+}
+
+)
